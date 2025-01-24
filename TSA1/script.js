ae3762fad0686ac641694d50967bda7b60251d0a
Jefferson Paul M. Petronio <151502808+Jeffersoncool0905@users.noreply.github.com>
2025-01-24 21:39:47 +0800

    script.js

--- TSA1/script.js
+++ TSA1/script.js
@@ -1,1 +1,36 @@
 
+function opentab(tabname) {
+    const tablinks = document.getElementsByClassName('tab-links');
+    const tabcontents = document.getElementsByClassName('tab-contents');
+
+    for (let i = 0; i < tablinks.length; i++) {
+        tablinks[i].classList.remove('tab-activate');
+    }
+    for (let i = 0; i < tabcontents.length; i++) {
+        tabcontents[i].classList.remove('block-activate');
+    }
+
+    document.querySelector(`.tab-links[onclick="opentab('${tabname}')"]`).classList.add('tab-activate');
+    document.getElementById(tabname).classList.add('block-activate');
+}
+
+function toggleColorPicker() {
+    const colorOptions = document.getElementById('colorOptions');
+    colorOptions.style.display = colorOptions.style.display === 'block' ? 'none' : 'block';
+}
+
+function changeColors(leftColor, rightColor, textColor) {
+    const leftSection = document.querySelector('.container-left');
+    const rightSection = document.querySelector('.container-right');
+    const name = document.querySelector('.name');
+    const course = document.querySelector('.course');
+    const infoItems = document.querySelectorAll('.info-item p');
+    const skillTexts = document.querySelectorAll('.skill p');
+
+    leftSection.style.backgroundColor = leftColor;
+    rightSection.style.backgroundColor = rightColor;
+    name.style.color = textColor;
+    course.style.color = textColor;
+    infoItems.forEach(item => item.style.color = textColor);
+    skillTexts.forEach(skill => skill.style.color = textColor);
+}
